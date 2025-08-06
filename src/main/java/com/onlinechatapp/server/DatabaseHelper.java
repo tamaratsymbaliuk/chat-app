e10f2389fa67440108edd653fc807e669fda5e75
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-08-05 22:00:30 -0500

    added initializeDatabse method

--- src/main/java/com/onlinechatapp/server/DatabaseHelper.java
+++ src/main/java/com/onlinechatapp/server/DatabaseHelper.java
@@ -1,7 +1,18 @@
 package com.onlinechatapp.server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DatabaseHelper {
 
     private static final String DB_URL =
             "jdbc:sqlite:C:\\Users\\kalas\\sql\\chat.sqlite";
+
+    static void initializeDatabase() {
+        try (Connection conn = DriverManager.getConnection(DB_URL); Statement stmt = conn.createStatement()) {
+            String userTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE);";
+            String messageTable = "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, message TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+        }
+    }
 }
