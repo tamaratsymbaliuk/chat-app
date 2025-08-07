b9a7d0152bd024731ea773ebf50be984fcca359f
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-08-06 22:28:55 -0500

    updated initialize method

--- src/main/java/com/onlinechatapp/server/DatabaseHelper.java
+++ src/main/java/com/onlinechatapp/server/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.onlinechatapp.server;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseHelper {
@@ -13,6 +14,10 @@ public class DatabaseHelper {
         try (Connection conn = DriverManager.getConnection(DB_URL); Statement stmt = conn.createStatement()) {
             String userTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE);";
             String messageTable = "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, message TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);";
+            stmt.execute(userTable);
+            stmt.execute(messageTable);
+        } catch (SQLException e) {
+            System.err.println("Error initializing database: " + e.getMessage());
         }
     }
 }
